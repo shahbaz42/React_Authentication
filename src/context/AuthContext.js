ecd7dc614bea4c9164a182833755dce901971807
Shahbaz <shahbaz_ali@outlook.in>
2022-12-05 18:07:38 +0530

    add authContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    // This use state is for the current user, It will store the current user
+    const [ currentUser, setCurrentUser ] = useState();
+    
+    // This is the signup function, it will create a new user with the email and password
+    function signup(email, password){
+        // This will return a promise, so we can use it in the signup component
+        return auth.createUserWithEmailAndPassword(email, password);
+    }
+
+    // onAuthStateChanged is a listener provided by firebase, 
+    // it will listen for any changes in the authentication state
+    useEffect(()=>{
+        const unsubscribe = auth.onAuthStateChanged( user => {
+            // using the useState setCurrent user function to set the current user.
+            setCurrentUser(user);
+        })
+        return unsubscribe;
+    }, [])
+
+    // This is the value that'll be passed to the AuthContext.Provider
+    const value = {
+        currentUser,
+        signup
+    }
+
+    // This will return the AuthContext.Provider with the value and the children
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
