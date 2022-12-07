038bc2347cd4c754dd0f9934405fefecfa9835e9
Shahbaz <shahbaz_ali@outlook.in>
2022-12-07 23:05:18 +0530

    add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,28 @@
-import Signup from './Signup';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../context/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup';
+import Dashboard from './Dashboard';
 
 function App() {
   return <>
-    <AuthProvider>
       <Container
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
       >
         <div className="w-100" style={{ maxWidth: "400px" }} >
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/" element={<Dashboard/>} />
+                <Route path="/signup" element={<Signup/>} />
+                <Route path="/login" element={<Login/>} />
+              </Routes>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   </>
 }
 
