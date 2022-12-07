af6421e80fbc126c24b087938c2e321a5f8b8c1f
Shahbaz <shahbaz_ali@outlook.in>
2022-12-08 00:10:23 +0530

    code login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Button, Card, Form, Alert } from 'react-bootstrap';
+import { useAuth } from '../context/AuthContext';
+import { Link } from "react-router-dom";
 
-export default function Login() {
+export default function Signup() {
+    // useRefs for storing the email and password 
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const { login } = useAuth()
+    // useState stores the error message
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    // This function will be called when the form is submitted
+    async function handleSubmit(e) {
+        // Prevents the page from reloading
+        e.preventDefault()
+        try {
+            setError('')
+            setLoading(true)
+            console.log("Attempting to log in...")
+            await login(emailRef.current.value, passwordRef.current.value); // to change to login
+            setLoading(false)  
+        } catch (error) {
+            setLoading(false);
+            setError('Failed to log in...')
+        }
+    }
+
   return (
-    <div>Login</div>
+    <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4">Login</h2>
+                { error && <Alert variant='danger' >{error}</Alert> }
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group id="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" ref={emailRef} required />
+                    </Form.Group>
+                    <Form.Group id="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} required />
+                    </Form.Group>
+                    <Button disabled={loading} className="w-100 mt-3" type="submit">
+                        {loading ? loadingAnimation() : "Login"}
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+        </div>
+    </>
   )
+}
+
+function loadingAnimation() {
+    return (
+        <div className="spinner-border text-light" role="status">
+            <span className="sr-only"></span>
+        </div>
+    )
 }
