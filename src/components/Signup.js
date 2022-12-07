26b404d12f8d3c08380980864a244da00bac8b8a
Shahbaz <shahbaz_ali@outlook.in>
2022-12-08 00:11:05 +0530

    add loading animation on button

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react'
 import { Button, Card, Form, Alert } from 'react-bootstrap';
 import { useAuth } from '../context/AuthContext';
+import { Link } from "react-router-dom";
 
 export default function Signup() {
     // useRefs for storing the email and password 
@@ -25,8 +26,8 @@ export default function Signup() {
         try {
             setError('')
             setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value); 
-            setLoading(false)  
+            await signup(emailRef.current.value, passwordRef.current.value);
+            setLoading(false)
         } catch (error) {
             console.log(error);
             setLoading(false);
@@ -34,34 +35,42 @@ export default function Signup() {
         }
     }
 
-  return (
-    <>
-        <Card>
-            <Card.Body>
-                <h2 className="text-center mb-4">Sign Up</h2>
-                { error && <Alert variant='danger' >{error}</Alert> }
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} required />
-                    </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} required />
-                    </Form.Group>
-                    <Form.Group id="password-confirm">
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" ref={passwordConfirmationRef} required />
-                    </Form.Group>
-                    <Button disabled={loading} className="w-100" type="submit">
-                        Sign Up
-                    </Button>
-                </Form>
-            </Card.Body>
-        </Card>
-        <div className="w-100 text-center mt-2">
-            Already have an account? 
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Sign Up</h2>
+                    {error && <Alert variant='danger' >{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required />
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="password" ref={passwordConfirmationRef} required />
+                        </Form.Group>
+                        <Button disabled={loading} className="w-100 mt-3" type="submit">
+                            {loading ? loadingAnimation() : "Sign Up"}
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? <Link to="/login">Log In</Link>
+            </div>
+        </>
+    )
+}
+
+function loadingAnimation() {
+    return (
+        <div className="spinner-border text-light" role="status">
+            <span className="sr-only"></span>
         </div>
-    </>
-  )
+    )
 }
