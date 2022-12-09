8bc03696cc70a4fae6c9dce829f2fd7c153fbb5e
Shahbaz <shahbaz_ali@outlook.in>
2022-12-10 03:09:00 +0530

    make dashboard route private

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
 import Dashboard from './Dashboard';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   return <>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }} >
-          <Router>
-            <AuthProvider>
-              <Routes>
-                <Route exact path="/" element={<Dashboard/>} />
-                <Route path="/signup" element={<Signup/>} />
-                <Route path="/login" element={<Login/>} />
-              </Routes>
-            </AuthProvider>
-          </Router>
-        </div>
-      </Container>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }} >
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact element={<PrivateRoute />}>
+                <Route exact path="/" element={<Dashboard />} />
+              </Route>
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   </>
 }
 
